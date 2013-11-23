7258319c986045cbed78131ac6e8b793da50e005
Marcin Szczepanski <marcins@gmail.com>
2013-11-23 21:00:40 +1100

    Cleanup ant script, improve CI helper shell script

--- tests/ci/scripts/ci-helper.sh
+++ tests/ci/scripts/ci-helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MXUNIT_FILE="mxunit-2.1.3.zip"
 if [ "$TRAVIS" == "true" ]; then
-	WORKDIR=$HOME
+	WORKDIR="$HOME/work"
 	RAILO_VER="railo-express-4.1.1.009-nojre"
 	RAILO_FILE="$RAILO_VER.tar.gz"
 	RAILO_URL=http://getrailo.com/railo/remote/download/4.1.1.009/railix/linux/$RAILO_FILE
@@ -9,24 +9,41 @@ if [ "$TRAVIS" == "true" ]; then
 else
 	# not TravisCI - local OSX testing
 	WORKDIR=/tmp/work
-	mkdir -p /tmp/work
-	rm -rf /tmp/work/*
 	RAILO_VER="railo-express-4.1.1.009-macosx"
 	RAILO_FILE="$RAILO_VER.zip"
 	RAILO_URL="http://localhost/$RAILO_FILE"
 	MXUNIT_URL="http://localhost/$MXUNIT_FILE"
 	TRAVIS_BUILD_DIR=`pwd`
 fi
+
 echo "Working directory: $WORKDIR"
+
+if [ -d $WORKDIR -a "$1" == "install" ]; then
+	rm -rf $WORKDIR
+fi
+
+mkdir -p $WORKDIR
 cd $WORKDIR
 
-# Download Railo Express
-wget $RAILO_URL
-if [[ "$RAILO_FILE" == *zip ]]; then
-	unzip $RAILO_FILE
-else
-	tar -zxvf $RAILO_FILE
-fi
-wget $MXUNIT_URL
-unzip $MXUNIT_FILE -d $RAILO_VER/webapps/www/
-ln -s $TRAVIS_BUILD_DIR $RAILO_VER/webapps/www/fw1
+case $1 in
+	install)
+		# Download Railo Express
+		wget $RAILO_URL
+		if [[ "$RAILO_FILE" == *zip ]]; then
+			unzip $RAILO_FILE
+		else
+			tar -zxvf $RAILO_FILE
+		fi
+		wget $MXUNIT_URL
+		unzip $MXUNIT_FILE -d $RAILO_VER/webapps/www/
+		ln -s $TRAVIS_BUILD_DIR $RAILO_VER/webapps/www/fw1
+		;;
+	start)
+		sh $RAILO_VER/start
+		;;
+	stop)
+		sh $RAILO_VER/stop
+		;;
+esac
+
+exit 0
